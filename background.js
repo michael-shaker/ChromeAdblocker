d678b011ccf2511e14c0a9dfcde3f440ed659515
Michael Shaker <michaelshakor@gmail.com>
2026-01-04 01:55:45 -0500

    Added some additional defenses for new websites

--- background.js
+++ background.js
@@ -9,6 +9,26 @@ const defaultFilters = [
 	"*://*.quantserve.com/*",
 	"*://*.scorecardresearch.com/*",
 	"*://*.zedo.com/*",
+	"*://*.adnxs.com/*",
+	"*://*.adsrvr.org/*",
+	"*://*.adservice.google.com/*",
+	"*://*.googletagservices.com/*",
+	"*://*.serving-sys.com/*",
+	"*://*.adform.net/*",
+	"*://*.adsafeprotected.com/*",
+	"*://*.pubmatic.com/*",
+	"*://*.rubiconproject.com/*",
+	"*://*.taboola.com/*",
+	"*://*.outbrain.com/*",
+	"*://*.adroll.com/*",
+	"*://*.ads-twitter.com/*",
+	"*://*.doubleverify.com/*",
+	"*://*.criteo.com/*",
+	"*://*.advertising.com/*",
+	"*://*.adserver.com/*",
+	"*://*.media.net/*",
+	"*://*.yieldmanager.com/*",
+	"*://*.pubmatic.com/*"
 ]
 
 chrome.webRequest.onBeforeRequest.addListener(
